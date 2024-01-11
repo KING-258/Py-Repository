206b35895a7c4e9f81782ee1c73dc5b7f72e27a7
KING-258 <amulyaparashar258@gmail.com>
2024-01-11 19:24:13 +0530

    Py-Repository

---
+++ Camera.py
@@ -0,0 +1,35 @@
+from telegram import Bot
+import asyncio
+import cv2
+import importlib
+import pywhatkit as pwk
+def main():
+    video_capture = cv2.VideoCapture(0)
+    previous_frame = None
+    while True:
+        ret, frame = video_capture.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+        if previous_frame is None:
+            previous_frame = gray
+            continue
+        frame_diff = cv2.absdiff(previous_frame, gray)
+        threshold = 30
+        _, thresh = cv2.threshold(frame_diff, threshold, 255, cv2.THRESH_BINARY)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            if cv2.contourArea(contour) < 1000:
+                continue
+            (x, y, w, h) = cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.imshow('Motion Detection', frame)
+        previous_frame = gray
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__" :
+    main()
